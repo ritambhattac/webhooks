7edeed4c3e7515ed1ad5ae11a857d994b674bc38
Ritam Bhattacr <bhattacharyyaritam91@gmail.com>
2024-03-08 16:26:52 +0530

    test

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+
 @app.post("/webhook")
 def check_payload(payload: dict):
     if "after" in payload:
-        print(dict)
+        print(list(dict.items()))
         return {"data": "there was a push event"}
-    
+
     raise HTTPException(status_code=400, detail="unable to get payload")
 
 
