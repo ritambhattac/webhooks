c30e3aae3923ae6780bfb1d5c9ec29826a736aea
Ritam Bhattacr <bhattacharyyaritam91@gmail.com>
2024-03-08 16:58:54 +0530

    test

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ def check_payload(payload: dict):
     if "after" in payload:
         print(payload)
         return {"data": "there was a push event"}
-
-
     raise HTTPException(status_code=400, detail="unable to get payload")
 
 
